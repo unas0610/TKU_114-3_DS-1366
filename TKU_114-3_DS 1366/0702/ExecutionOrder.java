cd2866d2f8cee8046be7dd2a5ffd666d1cffa166
unas0610 <yonas011085@gmail.com>
2026-07-02 10:50:18 +0800

    Add files via upload

---
+++ TKU_114-3_DS 1366/0702/ExecutionOrder.java
@@ -0,0 +1,7 @@
+public class ExecutionOrder {
+    public static void main(String[] args) {
+        System.out.println("Wake up");
+        System.out.println("Brush teeth");
+        System.out.println("Go to class");
+    }
+}
---
+++ TKU_114-3_DS 1366/0702/Hellojava.java
@@ -0,0 +1,7 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java .");
+        System.out.println("My name is 陳宥圻");
+        System.out.println("This is my Java practice.");
+    }
+}
