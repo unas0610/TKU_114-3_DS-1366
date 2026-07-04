0bea092ea15e176999314ac88bb9646848c84018
unas0610 <yonas011085@gmail.com>
2026-07-04 23:33:20 +0800

    Delete TKU_114-3_DS 1366 directory

--- TKU_114-3_DS 1366/0702/ExecutionOrder.java
+++
@@ -1,7 +0,0 @@
-public class ExecutionOrder {
-    public static void main(String[] args) {
-        System.out.println("Wake up");
-        System.out.println("Brush teeth");
-        System.out.println("Go to class");
-    }
-}
